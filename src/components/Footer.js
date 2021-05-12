27d3be8336547b6b85e0d579810fc5870c1eba81
Sim Brody <sim@simbrody.co.uk>
2021-05-12 10:50:55 +0100

    fixed routing in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import acfOptions from '../api/acfOptions';
-import baseUrl from '../api/baseUrl';
+
 
 const Footer = () => {
 
@@ -24,10 +24,10 @@ const Footer = () => {
         <footer>
             <div className="ui equal width grid">
                 <div className="column item bg-image-block" style={{ backgroundImage: `url(${footerData.image_left})` }}>
-                    <p><Link to={`${footerData.link_left.replace(baseUrl, '')}`}>{footerData.text_left}</Link></p>
+                    <p><Link to={footerData.link_left}>{footerData.text_left}</Link></p>
                 </div>
                 <div className="column item bg-image-block" style={{ backgroundImage: `url(${footerData.image_right})` }}>
-                    <p><Link to={`${footerData.link_right.replace(baseUrl, '')}`}>{footerData.text_right}</Link></p>
+                    <p><Link to={footerData.link_right}>{footerData.text_right}</Link></p>
                 </div>
             </div>
             <div className="ui center aligned container fluid segment lower-footer">
